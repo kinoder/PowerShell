c71695f603a1dfe3fa78960baeaff1cb0dc834c5
Armin Asgari <a.asgari@email.kntu.ac.ir>
2025-02-23 21:19:31 +0330

    added gorm and postgres packages and database.go file for connecting to database

---
+++ database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+const (
+	HOST   = "localhost"
+	USER   = "postgres"
+	PASS   = "armin"
+	DBNAME = "postgres"
+	PORT   = "5432"
+)
+
+func init() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", HOST, USER, PASS, DBNAME, PORT)
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("cannot connect to the database")
+	}
+}
