bec2c45634f1bec2e7ee76b1ca2b33aa12b7db60
Armin Asgari <a.asgari@email.kntu.ac.ir>
2025-02-23 15:27:00 +0330

    reading input and spliting it into arguments added.

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("$ ")
+		input, err := reader.ReadString('\n')
+		if err == nil {
+			args := strings.Fields(input)
+			if len(args) == 0 {
+				continue
+			}
+			
+		} else {
+			fmt.Println("cannot read input")
+		}
+
+	}
+}
+
+func exitCommand() {
 
 }
