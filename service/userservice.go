2ef633628470f6be48d9f7cc77b62d03b9b79140
Armin Asgari <a.asgari@email.kntu.ac.ir>
2025-02-25 17:19:53 +0330

    CreateUser() function  added to the project for servicing  adduser command

--- service/userservice.go
+++ service/userservice.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"errors"
+	models "hamkaran_system/bootcamp/final/project/model"
+
+	"gorm.io/gorm"
+)
+
+func CreateUser(db *gorm.DB, user *models.User) error {
+
+	if user.Password == "" {
+		return errors.New("please enter password")
+	}
+	if user.Username == "" {
+		return errors.New("please enter password")
+	}
+	var existingUser models.User
+	if err := db.Where("username = ?", user.Username).First(&existingUser).Error; err == nil {
+		return errors.New("username already exists")
+	}
+	if err := db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
